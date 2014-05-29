d1c22efba86e637c18b58685aab591848921ee6d
Jared Phillips <jaredtphillips@me.com>
2014-05-28 23:29:47 -0400

    blog styling but not quite cool enough

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$('.footer-link').click(function() {
-  $('.footer-link').removeClass("active");
-  return $(this).addClass("active");
-});
+(function() {
+	$('.footer-link').click(function() {
+	  $('.footer-link').removeClass("active");
+	  return $(this).addClass("active");
+	});
 
-$('.entry').click(function() {
-	$('p.article').removeClass("show");
-	return $('p.article', this).addClass("show");
-});
-
-// Git
+	$('.entry').click(function() {
+		$('p.article').removeClass("show");
+		return $('p.article', this).addClass("show");
+	});
+}).call(this);
